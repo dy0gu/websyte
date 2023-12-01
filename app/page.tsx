a30ea8e827db7cdb56ccb06520b2d9516c82493d
Dy0gu <dbsimoes20@gmail.com>
2023-12-01 01:00:14 +0000

    Lower particle staticity

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
 			<div className="hidden mx-16 w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<Particles
 				className="absolute inset-0 -z-10 animate-fade-in"
-				quantity={100}
 			/>
 			<h1 className="z-10 text-6xl md:text-7xl lg:text-8xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display bg-clip-text ">
 				<b>DY0GU</b>
