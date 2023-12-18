0258fa94004c43ec05b67f205b4b00c0f8d8733b
Dy0gu <dbsimoes20@gmail.com>
2023-12-18 02:41:34 +0000

    Update page name case

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,8 +2,8 @@ import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
 	return {
-		name: "Dy0gu",
-		short_name: "Dy0gu",
+		name: "dy0gu",
+		short_name: "dy0gu",
 		start_url: "/",
 		display: "standalone",
 		background_color: "#000000",
