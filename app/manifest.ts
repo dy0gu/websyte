b185edeeb2d7efd65b557b053d0b203d36ce6e25
Dy0gu <dbsimoes20@gmail.com>
2023-12-18 02:56:20 +0000

    Add prefered app metadata

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,11 +2,8 @@ import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
 	return {
-		name: "dy0gu",
-		short_name: "dy0gu",
 		start_url: "/",
 		display: "standalone",
-		background_color: "#000000",
-		theme_color: "#000000",
+		prefer_related_applications: false,
 	};
 }
