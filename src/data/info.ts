e8f593817826b3ab004f47129f77c729e863e38c
dy0gu <dbsimoes20@gmail.com>
2024-12-04 02:08:29 +0000

    chore: update description

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,7 +1,7 @@
 // Reusable values throughout the app
 const info = {
 	name: "Dy0gu",
-	description: "Is this line going to be shown under the website name?",
+	description: "Is this going to be under the website name?",
 	links: [
 		{ name: "GitHub", href: "https://www.github.com/dy0gu" },
 		{
