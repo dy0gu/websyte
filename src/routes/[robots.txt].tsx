cb9b63289adc5d21594cf231c6e95cbea30286cb
dy0gu <dbsimoes20@gmail.com>
2024-12-05 17:18:13 +0000

    fix: update robot allowance

--- src/routes/[robots.txt].tsx
+++ src/routes/[robots.txt].tsx
@@ -4,8 +4,7 @@ import { env } from "~/utils/env";
 // Dynamic robots.txt generator route
 export function loader() {
 	return txt(`User-agent: *
-Allow: /$
-Disallow: /
+Disallow:
 
 Sitemap: ${env.DOMAIN}/sitemap.xml`);
 }
